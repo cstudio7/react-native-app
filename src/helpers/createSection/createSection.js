b2d80716315cba67fb905a0c396b0666c91272fe
artiebits <artur.khusaenov@gmail.com>
2018-08-25 18:06:10 +0200

    fix(createSection): replace Lodash with Ramda

--- src/helpers/createSection/createSection.js
+++ src/helpers/createSection/createSection.js
@@ -1,7 +1,8 @@
-import { groupBy } from 'lodash';
+const R = require('ramda');
 
 const createSection = names => {
-  const groups = groupBy(names, nameObj => nameObj.name.substr(0, 1));
+  const groupByFirstLetter = R.groupBy(nameObj => nameObj.name.substr(0, 1));
+  const groups = groupByFirstLetter(names);
   return Object.keys(groups).map(groupName => ({
     title: groupName,
     data: groups[groupName]
