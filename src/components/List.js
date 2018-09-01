b2fcc350d8b7bf8039c97c944bee630d3d213322
Sveta Khannanova <ifrodia@gmail.com>
2018-09-01 20:43:50 +0200

    feat(ListSectionHeader): add style

--- src/components/List.js
+++ src/components/List.js
@@ -3,9 +3,10 @@ import { Text, SectionList, StyleSheet } from 'react-native';
 import createSection from '../modules/createSection/createSection';
 import ListItem from '../containers/ListItem';
 import Spacing from '../constants/Spacing';
+import ListSectionHeader from './ListSectionHeader'
 
 class List extends React.Component {
-  renderSectionHeader = ({ section: { title } }) => <Text>{title}</Text>;
+  renderSectionHeader = ({ section: { title } }) => <ListSectionHeader title={title} />;
 
   renderItem = ({ item }) => (
     <ListItem name={item} gender={this.props.gender} page={this.props.page} />
---
+++ src/components/ListSectionHeader.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+import Spacing from '../constants/Spacing';
+
+const ListHeaderSection = ({ title }) => (
+  <Text style={styles.header}>{title}</Text>
+);
+
+const styles = StyleSheet.create({
+  header: {
+    color: '#979797',
+    fontSize: 15,
+    lineHeight: 18,
+    backgroundColor: '#fff',
+    paddingBottom: Spacing.padding
+  }
+});
+
+export default ListHeaderSection;
