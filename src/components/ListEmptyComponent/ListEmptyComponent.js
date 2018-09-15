04bb5fe67f1004c42fa319235351c4ad10b74cfa
Sveta Khannanova <ifrodia@gmail.com>
2018-09-15 11:59:29 +0200

    feat(ListEmptyComponent): add style

--- src/components/ListEmptyComponent/ListEmptyComponent.js
+++ src/components/ListEmptyComponent/ListEmptyComponent.js
@@ -1,8 +1,26 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+import { Colors } from '../../constants';
 
 const ListEmptyComponent = () => (
-  <Text>Empty list</Text>
+  <View style={styles.view}>
+    <Text style={styles.text}>Вы пока ничего не добавили в избранное</Text>
+  </View>
 );
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 50
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: '500',
+    color: Colors.secondary
+  }
+});
 
 export default ListEmptyComponent;
