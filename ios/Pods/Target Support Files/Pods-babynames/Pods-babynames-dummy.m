d53bec81e619843bd5bd652cdb0687bcdbed4000
artiebits <artur.khusaenov@gmail.com>
2018-10-06 12:17:04 +0200

    feat(ios): add Amplitude back

---
+++ ios/Pods/Target Support Files/Pods-babynames/Pods-babynames-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_babynames : NSObject
+@end
+@implementation PodsDummy_Pods_babynames
+@end
---
+++ ios/Pods/Target Support Files/Pods-babynamesTests/Pods-babynamesTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_babynamesTests : NSObject
+@end
+@implementation PodsDummy_Pods_babynamesTests
+@end
