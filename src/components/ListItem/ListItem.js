97eb09f5d7b7241ce2801afa5ec5099c7205b7de
artiebits <artur.khusaenov@gmail.com>
2018-12-03 22:57:12 +0100

    refactor(ListItem): use functional component

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -3,39 +3,34 @@ import { TouchableOpacity, StyleSheet } from 'react-native';
 import { Body, ListItem, Text, Right, Icon } from 'native-base';
 import { Colors } from '../../constants';
 
-class ListItemComponent extends React.PureComponent {
-  render() {
-    const { name, favorite, gender } = this.props;
-    return (
-      <ListItem>
-        <Body>
-          <Text style={styles.listText}>{name.name}</Text>
-          <Text style={styles.listText} note numberOfLines={1}>
-            {name.meaning}
-          </Text>
-        </Body>
-        <Right>
-          <TouchableOpacity
-            onPress={() => {
-              const payload = {
-                gender,
-                name: {
-                  ...name,
-                  isFavorite: !name.isFavorite
-                }
-              };
-              favorite(payload);
-            }}>
-            <Icon
-              name={`md-heart${name.isFavorite ? '' : '-outline'}`}
-              style={styles.icon}
-            />
-          </TouchableOpacity>
-        </Right>
-      </ListItem>
-    );
-  }
-}
+const ListItemComponent = ({ name, favorite, gender }) => (
+  <ListItem>
+    <Body>
+      <Text style={styles.listText}>{name.name}</Text>
+      <Text style={styles.listText} note numberOfLines={1}>
+        {name.meaning}
+      </Text>
+    </Body>
+    <Right>
+      <TouchableOpacity
+        onPress={() => {
+          const payload = {
+            gender,
+            name: {
+              ...name,
+              isFavorite: !name.isFavorite
+            }
+          };
+          favorite(payload);
+        }}>
+        <Icon
+          name={`md-heart${name.isFavorite ? '' : '-outline'}`}
+          style={styles.icon}
+        />
+      </TouchableOpacity>
+    </Right>
+  </ListItem>
+);
 
 const styles = StyleSheet.create({
   listText: {
