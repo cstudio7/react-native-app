0f0aeaadd581b67d94226334efe530a67c9d7dbe
artiebits <artur.khusaenov@gmail.com>
2019-02-19 19:38:22 +0100

    fix: move screens to containers

--- src/containers/FavoritesScreen.js
+++ src/containers/FavoritesScreen.js
@@ -1,8 +1,51 @@
+import React from 'react';
+import { Keyboard } from 'react-native';
 import { connect } from 'react-redux';
+import { withNavigationFocus } from 'react-navigation';
+import { Tab, Tabs, Container } from 'native-base';
 import { favoritesScreenView } from '../actions/actions';
-import FavoritesScreen from '../screens/FavoritesScreen';
+import List from '../components/List/List';
+import ListEmptyComponent from '../components/ListEmptyComponent/ListEmptyComponent';
 
-const mapStateToProps = (state) => ({
+const dismissKeyboard = () => Keyboard.dismiss();
+
+const FavoritesScreen = ({ data, isFocused, favoritesScreenView }) => {
+  isFocused && favoritesScreenView();
+  return (
+    <Container>
+      <Tabs onChangeTab={dismissKeyboard}>
+        <Tab heading="Девочки">
+          <List
+            tab="female"
+            screen="Favorites"
+            data={getFavoriteNames(data, 'female')}
+            renderListEmptyComponent={renderListEmptyComponent}
+          />
+        </Tab>
+        <Tab heading="Мальчики">
+          <List
+            tab="male"
+            screen="Favorites"
+            data={getFavoriteNames(data, 'male')}
+            renderListEmptyComponent={renderListEmptyComponent}
+          />
+        </Tab>
+      </Tabs>
+    </Container>
+  );
+};
+
+function getFavoriteNames(names, tab) {
+  return names[tab].filter(name => name.isFavorite);
+}
+
+function renderListEmptyComponent() {
+  return (
+    <ListEmptyComponent message="Вы пока ничего не добавили в избранное" />
+  );
+}
+
+const mapStateToProps = state => ({
   data: state.names
 });
 
@@ -11,4 +54,4 @@ export default connect(
   {
     favoritesScreenView
   }
-)(FavoritesScreen);
+)(withNavigationFocus(FavoritesScreen));
--- src/containers/ListScreen.js
+++ src/containers/ListScreen.js
@@ -1,10 +1,44 @@
+import React from 'react';
+import { Keyboard } from 'react-native';
 import { connect } from 'react-redux';
+import { withNavigationFocus } from 'react-navigation';
+import { Container, Tab, Tabs } from 'native-base';
 import {
   listScreenView,
   loadNames,
   updateUserVisits
 } from '../actions/actions';
-import ListScreen from '../screens/ListScreen';
+import ListWithSearchForm from '../containers/ListWithSearchForm';
+import RateAlert from '../containers/RateAlert';
+
+const dismissKeyboard = () => Keyboard.dismiss();
+
+class ListScreen extends React.PureComponent {
+  componentDidMount() {
+    this.props.loadNames();
+    this.props.updateUserVisits();
+  }
+
+  render() {
+    if (this.props.isFocused) {
+      this.props.listScreenView();
+    }
+
+    return (
+      <Container>
+        <RateAlert />
+        <Tabs onChangeTab={dismissKeyboard}>
+          <Tab heading="Девочки">
+            <ListWithSearchForm tab="female" screen="List" />
+          </Tab>
+          <Tab heading="Мальчики">
+            <ListWithSearchForm tab="male" screen="List" />
+          </Tab>
+        </Tabs>
+      </Container>
+    );
+  }
+}
 
 const mapStateToProps = () => ({});
 
@@ -15,4 +49,4 @@ export default connect(
     loadNames,
     updateUserVisits
   }
-)(ListScreen);
+)(withNavigationFocus(ListScreen));
